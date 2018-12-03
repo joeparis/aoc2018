84e48527ec5c64698d0564b09a1500e5665a294d
Joe Paris <joe.paris@outlook.com>
2018-12-02 19:11:08 -0800

    Removed unnecessary code, added timing.

--- day1/part1.py
+++ day1/part1.py
@@ -1,13 +1,16 @@
 #! python3
 # coding=utf-8
+from time import time
 
 
 def main():
-    deltas = []
     with open("input.txt", "r") as file:
         deltas = [int(value) for value in file.read().strip().split()]
     print(sum(deltas))
 
 
 if __name__ == "__main__":
+    start = time()
     main()
+    end = time()
+    print(end - start)
